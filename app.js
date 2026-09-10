b5dd85bc8e942e040863de8396f44274f46c612f
Arun IGIS <arunkumar.v@infogsolution.com>
2026-09-10 18:03:46 +0530

    Test CI pipeline

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("DevOps Demo Application is running!");
+  res.send("DevOps Demo Application is running! CI/CD Test!!");
 });
 
 if (require.main === module) {
